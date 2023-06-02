686a305ca82d1e3db83146526b2741bde56a8599
Hustleflow <banados.jg@gmail.com>
2023-06-02 09:33:14 -0500

    aPackageUsed

---
+++ src/Components/Digital.js
@@ -0,0 +1,11 @@
+import { Clock } from "digital-clock-react";
+
+const ThisClock = () => {
+  return (
+    <div className="app-container">
+      <Clock/>
+    </div>
+  );
+}
+
+export default ThisClock;
